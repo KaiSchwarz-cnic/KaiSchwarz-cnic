15d049b73ae02adfdcf2a1420dc4c622033c88a9
Kai Schwarz <kschwarz@hexonet.net>
2021-02-10 17:43:27 +0100

    feat(knowledge cloud): added

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,33 @@
 $(window).on('load', function () {
     $('.level-bar-inner').each(function () {
-        var itemWidth = $(this).data('level');
+        var itemWidth = $(this).data('level')
         $(this).animate({
             width: itemWidth
-        }, 800);
-    });
-});
+        }, 800)
+    })
+})
 
-
-jQuery(document).ready(function ($) {
-
+$(function ($) {
     /*======= Skillset *=======*/
-    $('.level-bar-inner').css('width', '0');
+    $('.level-bar-inner').css('width', '0')
 
     /* Bootstrap Tooltip for Skillset */
-    $('.level-label').tooltip();
-});
+    $('.level-label').tooltip()
+
+    $('.hoverimg')
+        .off()
+        .on('mouseenter', function(e){
+            const img = $(this)
+            const x = e.pageX - this.naturalWidth
+            const y = e.pageY - this.naturalHeight + 100
+            $("body").append(`<div id="hoverimg"><img src="${img.attr('src')}"/></div>`)
+            $("#hoverimg")
+                .css("position", "absolute")
+                .css("top", `${y}px`)
+                .css("left", `${x}px`)
+                .fadeIn("fast")
+        })
+        .on('mouseleave', function(){
+            $("#hoverimg").remove()
+        })
+})
